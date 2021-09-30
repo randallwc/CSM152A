763b856f4f878d3c9eee05c93cf0e40183aff00a
William Randall <wrandall1000@gmail.com>
2021-09-30 12:34:26 -0700

    fixes for counter TB

--- lab0.1/src/4_bit_counter_TB.v
+++ lab0.1/src/4_bit_counter_TB.v
@@ -1,29 +1,11 @@
-/*
- * Module: Testbench for multiplexed combinational gates
- * 
- * Filename: combinational_gates_muxed_TB.v
- * Version: 1.0
- *
- * Author: Cejo Konuparamban Lonappan
- *
- * Description: The testbench code for verifying the multiplexed outputs of
- * eight comnibational gates. 
- */
-
 `timescale 1ns / 1ps
 
-module 4_bit_counter_tb; // No inputs for a testbench!
+module 4_bit_counter_tb;
 
-// Inputs in the module to be tested will be port mapped to register variables
 reg a0_tb, a1_tb, a2_tb, a3_tb;
 
-// Instantiation of the design module to be verified by the testbench
-// Use named portmapping to map inputs to regsiter variables and outputs to
-// wires
-4_bit_counter UUT (.clk(clk_tb), .rst(rst_tb), .a0((a0_tb), .a1(a1_tb), .a2(a2_tb), .a3(a3_tb));
+4_bit_counter UUT (.clk(clk_tb), .rst(rst_tb), .a0(a0_tb), .a1(a1_tb), .a2(a2_tb), .a3(a3_tb));
 
-// IMPORTANT: Initialize all inputs. Otherwise the default value of register
-// will be don't care (x).
 initial
 begin
 	clk_tb = '1b0;
@@ -38,7 +20,6 @@ end
 always
 	#5 clk_tb = ~clk_tb;
 
-// Code to terminate simulation after all the test cases have been covered.
 initial
 	#160 $finish; // After 160 timeunits, terminate simulation.
 
