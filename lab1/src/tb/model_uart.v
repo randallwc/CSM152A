efac07cda62fb1fdb77a96245f5254106d3f1b58
William Randall <15082470+randallwc@users.noreply.github.com>
2021-10-14 01:56:19 -0700

    add comments and remove comments and fix whitespace

--- lab1/src/tb/model_uart.v
+++ lab1/src/tb/model_uart.v
@@ -1,80 +1,80 @@
 `timescale 1ns / 1ps
 
 module model_uart(/*AUTOARG*/
-   // Outputs
-   TX,
-   // Inputs
-   RX
-   );
+    // Outputs
+    TX,
+    // Inputs
+    RX
+    );
 
-   output TX;
-   input  RX;
+    output TX;
+    input  RX;
 
-   parameter baud    = 115200;
-   parameter bittime = 1000000000/baud;
-   parameter name    = "UART0";
-   
-   reg [7:0] rxData;
-	reg [31:0] rxBuffer; // hold 8 bits
-   event     evBit;
-   event     evByte;
-   event     evTxBit;
-   event     evTxByte;
-   reg       TX;
+    parameter baud    = 115200;
+    parameter bittime = 1000000000/baud;
+    parameter name    = "UART0";
 
-   initial
-     begin
+    reg [7:0] rxData;
+    reg [31:0] rxBuffer; // hold 8 bits
+    event     evBit;
+    event     evByte;
+    event     evTxBit;
+    event     evTxByte;
+    reg       TX;
+
+    initial
+    begin
         TX = 1'b1;
-     end
-   
-   always @ (negedge RX)
-     begin
+    end
+
+    always @ (negedge RX)
+    begin
         rxData[7:0] = 8'h0;
         #(0.5*bittime);
-		   
+
         repeat (8) // 8 bits == 1 byte
-          begin
-             #bittime ->evBit;
-             //rxData[7:0] = {rxData[6:0],RX};
-             rxData[7:0] = {RX,rxData[7:1]}; // right shift based on RX bit where RX is the register
-          end
-			if (rxData[7:0] != 8'h0a && rxData[7:0] != 8'h0d)
-				begin
-					rxBuffer[31:0] = {rxBuffer[24:0],rxData[7:0]}; // shift by 8
-				end
-			else
-				begin
-					if (rxData[7:0] == 8'h0d)
-						begin
-							$display ("%d %s Received bytes %08x (%s)", $stime, name, rxBuffer, rxBuffer);
-						end
-				end
+        begin
+            #bittime ->evBit;
+            //rxData[7:0] = {rxData[6:0],RX};
+            rxData[7:0] = {RX,rxData[7:1]}; // right shift based on RX bit where RX is the register
+        end
+        if (rxData[7:0] != 8'h0a && rxData[7:0] != 8'h0d)
+        begin
+            rxBuffer[31:0] = {rxBuffer[24:0],rxData[7:0]}; // shift by 8
+        end
+        else
+        begin
+            if (rxData[7:0] == 8'h0d)
+            begin
+                $display ("%d %s Received bytes %08x (%s)", $stime, name, rxBuffer, rxBuffer);
+            end
+        end
         ->evByte;
         // $display ("%d %s Received byte %02x (%s)", $stime, name, rxData, rxData);
-     end
+    end
 
-   task tskRxData;
-      output [7:0] data;
-      begin
-         @(evByte);
-         data = rxData;
-      end
-   endtask // for
+    task tskRxData;
+    output [7:0] data;
+    begin
+        @(evByte);
+        data = rxData;
+    end
+    endtask
       
-   task tskTxData;
-      input [7:0] data;
-      reg [9:0]   tmp;
-      integer     i;
-      begin
-         tmp = {1'b1, data[7:0], 1'b0};
-         for (i=0;i<10;i=i+1)
-           begin
-              TX = tmp[i];
-              #bittime;
-              ->evTxBit;
-           end
-         ->evTxByte;
-      end
-   endtask // tskTxData
-   
+    task tskTxData;
+    input [7:0] data;
+    reg [9:0]   tmp;
+    integer     i;
+    begin
+        tmp = {1'b1, data[7:0], 1'b0};
+        for (i=0;i<10;i=i+1)
+        begin
+            TX = tmp[i];
+            #bittime;
+            ->evTxBit;
+        end
+        ->evTxByte;
+    end
+    endtask // tskTxData
+
 endmodule // model_uart
--- lab1/src/tb/tb.v
+++ lab1/src/tb/tb.v
@@ -2,148 +2,147 @@
 
 module tb;
 
-   reg [7:0] sw;
-   reg       clk;
-   reg       btnS;
-   reg       btnR;
-	
-	// Stuff for SEQ.CODE
-	integer seq_file;
-	integer seq_line;
-	reg[7:0] instruction_line;
-   
-   integer   i;
-   
-   /*AUTOWIRE*/
-   // Beginning of automatic wires (for undeclared instantiated-module outputs)
-   wire                 RsRx;                   // From model_uart0_ of model_uart.v
-   wire                 RsTx;                   // From uut_ of nexys3.v
-   wire [7:0]           led;                    // From uut_ of nexys3.v
-   // End of automatics
+reg [7:0] sw;
+reg       clk;
+reg       btnS;
+reg       btnR;
 
-   initial
-     begin
-        //$shm_open  ("dump", , ,1);
-        //$shm_probe (tb, "ASTF");
+// Stuff for SEQ.CODE
+integer seq_file;
+integer seq_line;
+reg[7:0] instruction_line;
 
-        clk = 0;
-        btnR = 1;
-        btnS = 0;
-		  seq_file = 0;
-        #1000 btnR = 0;
-        #1500000;
+integer   i;
 
-		  seq_file = $fopen("/home/ise/CS_M152A/Lab 1/fib.code", "r");
-		  if (seq_file == 0) 
-			begin
-				$display("FILE READ ERROR");
-				$finish;
-			end
-		  
-		  seq_line = $fscanf(seq_file, "%b\n", instruction_line);
-        
-        #1000;        
-     end
-	  
-	always @(posedge clk)
-		begin
-			if (seq_file != 0)
-				begin
-				if (!$feof(seq_file))
-					begin
-						seq_line = $fscanf(seq_file, "%b\n", instruction_line);
-						tskRunInst(instruction_line);
-						$display ("Instruction: [%b]", instruction_line);
-					end
-				else
-					begin
-						$finish;
-						$fclose(seq_file);
-					end
-			end
-		end
+/*AUTOWIRE*/
+// Beginning of automatic wires (for undeclared instantiated-module outputs)
+wire                 RsRx;                   // From model_uart0_ of model_uart.v
+wire                 RsTx;                   // From uut_ of nexys3.v
+wire [7:0]           led;                    // From uut_ of nexys3.v
+// End of automatics
 
-   always #5 clk = ~clk;
-   
-   model_uart model_uart0_ (// Outputs
-                            .TX                  (RsRx),
-                            // Inputs
-                            .RX                  (RsTx)
-                            /*AUTOINST*/);
+initial
+begin
+    clk = 0;
+    btnR = 1;
+    btnS = 0;
+    seq_file = 0;
+    #1000 btnR = 0;
+    #1500000;
+    seq_file = $fopen("/home/ise/CS_M152A/Lab 1/fib.code", "r");
+    if (seq_file == 0) 
+    begin
+        $display("ERROR: FILE READ ERROR");
+        $finish;
+    end
+    seq_line = $fscanf(seq_file, "%b\n", instruction_line); // read first line and disgard -- TODO remove
+    // TODO parse number
+    #1000;        
+end
+  
+always @(posedge clk)
+begin
+    if (seq_file != 0)
+    begin
+        // if not end of file
+        // TODO: and not past given length
+        if (!$feof(seq_file))
+        begin
+            // read line
+            seq_line = $fscanf(seq_file, "%b\n", instruction_line);
+            // run instruction
+            tskRunInst(instruction_line);
+            // display instruction
+            $display ("DEBUG: Instruction: [%b]", instruction_line);
+        end
+        else
+        begin
+            $finish;
+            $fclose(seq_file);
+        end
+    end
+end
 
-   defparam model_uart0_.name = "UART0";
-   defparam model_uart0_.baud = 1000000;
-   
-   
-   nexys3 uut_ (/*AUTOINST*/
-                // Outputs
-                .RsTx                   (RsTx),
-                .led                    (led[7:0]),
-                // Inputs
-                .RsRx                   (RsRx),
-                .sw                     (sw[7:0]),
-                .btnS                   (btnS),
-                .btnR                   (btnR),
-                .clk                    (clk));
+always #5 clk = ~clk;
 
-   task tskRunInst;
-      input [7:0] inst;
-      begin
-         $display ("%d ... Running instruction %08b", $stime, inst);
-         sw = inst;
-         #1500000 btnS = 1;
-         #3000000 btnS = 0;
-      end
-   endtask //
+model_uart model_uart0_ (// Outputs
+                        .TX                  (RsRx),
+                        // Inputs
+                        .RX                  (RsTx)
+                        /*AUTOINST*/);
 
-   task tskRunPUSH;
-      input [1:0] ra;
-      input [3:0] immd;
-      reg [7:0]   inst;
-      begin
-         inst = {2'b00, ra[1:0], immd[3:0]};
-         tskRunInst(inst);
-      end
-   endtask //
+defparam model_uart0_.name = "UART0";
+defparam model_uart0_.baud = 1000000;
 
-   task tskRunSEND;
-      input [1:0] ra;
-      reg [7:0]   inst;
-      begin
-         inst = {2'b11, ra[1:0], 4'h0};
-         tskRunInst(inst);
-      end
-   endtask //
+nexys3 uut_ (/*AUTOINST*/
+            // Outputs
+            .RsTx                   (RsTx),
+            .led                    (led[7:0]),
+            // Inputs
+            .RsRx                   (RsRx),
+            .sw                     (sw[7:0]),
+            .btnS                   (btnS),
+            .btnR                   (btnR),
+            .clk                    (clk));
 
-   task tskRunADD;
-      input [1:0] ra;
-      input [1:0] rb;
-      input [1:0] rc;
-      reg [7:0]   inst;
-      begin
-         inst = {2'b01, ra[1:0], rb[1:0], rc[1:0]};
-         tskRunInst(inst);
-      end
-   endtask //
+task tskRunInst;
+    input [7:0] inst;
+    begin
+        $display ("%d ... Running instruction %08b", $stime, inst);
+        sw = inst;
+        #1500000 btnS = 1;
+        #3000000 btnS = 0;
+    end
+endtask //
 
-   task tskRunMULT;
-      input [1:0] ra;
-      input [1:0] rb;
-      input [1:0] rc;
-      reg [7:0]   inst;
-      begin
+task tskRunPUSH;
+    input [1:0] ra;
+    input [3:0] immd;
+    reg [7:0]   inst;
+    begin
+        inst = {2'b00, ra[1:0], immd[3:0]};
+        tskRunInst(inst);
+    end
+endtask //
+
+task tskRunSEND;
+    input [1:0] ra;
+    reg [7:0]   inst;
+    begin
+        inst = {2'b11, ra[1:0], 4'h0};
+        tskRunInst(inst);
+    end
+endtask //
+
+task tskRunADD;
+    input [1:0] ra;
+    input [1:0] rb;
+    input [1:0] rc;
+    reg [7:0]   inst;
+    begin
+        inst = {2'b01, ra[1:0], rb[1:0], rc[1:0]};
+        tskRunInst(inst);
+    end
+endtask //
+
+task tskRunMULT;
+    input [1:0] ra;
+    input [1:0] rb;
+    input [1:0] rc;
+    reg [7:0]   inst;
+    begin
          inst = {2'b10, ra[1:0], rb[1:0], rc[1:0]};
          tskRunInst(inst);
-      end
-   endtask //
+    end
+endtask //
 
-   always @ (posedge clk)
-     if (uut_.inst_vld)
-       $display("%d ... instruction %08b executed", $stime, uut_.inst_wd);
+always @ (posedge clk)
+    if (uut_.inst_vld)
+        $display("%d ... instruction %08b executed", $stime, uut_.inst_wd);
 
-   always @ (led)
-     $display("%d ... led output changed to %08b", $stime, led);
-   
+always @ (led)
+    $display("%d ... led output changed to %08b", $stime, led);
+
 endmodule // tb
 // Local Variables:
 // verilog-library-flags:("-y ../src/")
