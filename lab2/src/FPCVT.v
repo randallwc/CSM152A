e7edc684905ab5feb9016ae1ed2919a7f76bc67f
William Randall <15082470+randallwc@users.noreply.github.com>
2021-10-21 05:09:54 -0400

    working FPCVT

--- lab2/src/FPCVT.v
+++ lab2/src/FPCVT.v
@@ -1,211 +1,178 @@
-module nexys3 (/*AUTOARG*/
-   // Outputs
-   sign, exponent, significand,
-   // Inputs
-   analog, clk
-   );
-      
-   // Misc.
-   input  [11:0] analog;
-   output        sign;
-	output [2:0]  exponent;
-   output [3:0]  significand;
-   
-   // Logic
-   input         clk;                  // 100MHz
-   
-   /*AUTOWIRE*/
-   // Beginning of automatic wires (for undeclared instantiated-module outputs)
-   wire        convert_sign;        // From converter module
-   wire [11:0] convert_magnitude;   // From converter module
-	wire [2:0]  count_exponent;      // From zero-counter
-	wire [3:0]  extract_significand; // From extracter
-	wire        extract_fifth;       // From extracter
-	wire [2:0]  round_exponent;    // From rounder
-	wire [3:0]  round_significand; // From rounder
-   // End of automatics
-   
-	// Add more registers as needed
-	// May need to add validation logic
-	
-	assign sign = convert_sign;
-	assign exponent = round_exponent;
-	assign significand = round_significand;
-	
-   // ===========================================================================
-   // Converter
-   // ===========================================================================
+`timescale 1ns / 1ps
 
-   converter converter_instance (// Outputs
-             .sign                 (convert_sign),
-             .magnitude            (convert_magnitude),
-             // Inputs
-             .analog               (analog),
-             /*AUTOINST*/
-             // Inputs
-             .clk                  (clk));
-   
-   // ===========================================================================
-   // Counter
-   // ===========================================================================
+// 12 bits to floating point
+//   7  |   6 5 4  | 3 2 1 0
+//  sign| exponent | significand
+module FPCVT(
+    //Input data in Two's Complement Representation.
+    input [11:0] D,
+    // Sign bit of the Floating Point Representation.
+    output S,
+    // 3-Bit Exponent of the Floating Point Representation.
+    output [2:0] E,
+    // 4-Bit Significand of the Floating Point Representation.
+    output [3:0] F
+    );
+    
+    //create wires
+    wire [11:0] m_abs_D; 
+    wire [2:0] m_E;
+    wire [3:0] m_F;
+    wire m_sig_bit;
+    wire out_S;
+    wire [2:0] out_E;
+    wire [3:0] out_F;
 
-   counter counter_instance (// Outputs
-             .exponent                 (count_exponent),
-             // Inputs
-             .magnitude                (convert_magnitude),
-             /*AUTOINST*/
-             // Inputs
-             .clk                      (clk));
-   
-   // ===========================================================================
-   // Extracter
-   // ===========================================================================
+    // create 3 modules
+    // sign module
+    getSign m_getSign(
+    .in_D(D),
+    .out_S(out_S),
+    .out_abs_D(m_abs_D)
+    );
+    // convert float module
+    getConvertedFloat m_getConvertedFloat(
+    .in_abs_D(m_abs_D),
+    .out_E(m_E),
+    .out_F(m_F),
+    .out_sig_bit(m_sig_bit
+    ));
+    // round module
+    getRoundedFloat m_getRoundedFloat(
+    .in_E(m_E),
+    .in_F(m_F),
+    .in_sig_bit(m_sig_bit),
+    .out_E(out_E),
+    .out_F(out_F)
+    );
+    
+    assign S = out_S;
+    assign E = out_E;
+    assign F = out_F;
 
-   extracter extracter_instance (// Outputs
-             .significand          (extract_significand),
-             .fifth                (extract_fifth),
-             // Inputs
-             .magnitude            (convert_magnitude),
-             /*AUTOINST*/
-             // Inputs
-             .clk                  (clk));
-				 
-   // ===========================================================================
-   // Rounder
-   // ===========================================================================
+endmodule
 
-   rounder rounder_instance (// Outputs
-             .o_exponent           (round_exponent),
-             .o_significand        (round_significand),
-             // Inputs
-             .i_exponent           (count_exponent),
-             .i_significand        (extract_significand),
-             .i_fifth              (extract_fifth),
-             /*AUTOINST*/
-             // Inputs
-             .clk                       (clk));
-   
-endmodule // nexys3
-// Local Variables:
-// verilog-library-flags:("-f ../input.vc")
-// End:
+// change 12 bit 2's compliment into sign magnitude representation
+module getSign(
+    input [11:0] in_D,
+    output out_S,
+    output reg [11:0] out_abs_D
+    );
+    
+    assign out_S = in_D[11];
+    
+    always @(*)
+    begin
+        out_abs_D = in_D;
+        if ( in_D[11] == 1 )
+        begin
+            out_abs_D = ~in_D + 1;
+            if (out_abs_D[11] == 1)
+            begin
+                // edge case for most negative value 10...0
+                out_abs_D = ~out_abs_D;
+            end
+        end
+    end
+endmodule
 
-module converter (/*AUTOARG*/
-   // Outputs
-   sign, magnitude,
-   // Inputs
-   analog, clk
-   );
-	
-	// Misc.
-   input  [11:0] analog;
-   output        sign;
-	output [11:0] magnitude;
-	
-	reg [11:0] flipped_analog;
-   
-   // Logic
-   input         clk;                  // 100MHz
-	
-	// Assignment
-	assign flipped_analog = !analog;
-	assign magnitude = flipped_analog + 1;
-	
-	assign sign = analog[11];
-	
-endmodule // converter
+// change 12 bit number with a sign bit to float 
+// and has output for significant bit after significand
+module getConvertedFloat(
+    input [11:0] in_abs_D,
+    output reg [2:0] out_E,
+    output reg [3:0] out_F,
+    output reg out_sig_bit
+    );
+    
+    always @(*)
+    begin
+        casez ( in_abs_D )
+            12'b01zz_zzzz_zzzz:
+            begin
+                out_E = 3'b111;
+                out_F = in_abs_D[10:7];
+                out_sig_bit = in_abs_D[6];
+            end
+            12'b001z_zzzz_zzzz:
+            begin
+                out_E = 3'b110;
+                out_F = in_abs_D[9:6];
+                out_sig_bit = in_abs_D[5];
+            end
+            12'b0001_zzzz_zzzz:
+            begin
+                out_E = 3'b101;
+                out_F = in_abs_D[8:5];
+                out_sig_bit = in_abs_D[4];
+            end
+            12'b0000_1zzz_zzzz:
+            begin
+                out_E = 3'b100;
+                out_F = in_abs_D[7:4];
+                out_sig_bit = in_abs_D[3];
+            end
+            12'b0000_01zz_zzzz:
+            begin
+                out_E = 3'b011;
+                out_F = in_abs_D[6:3];
+                out_sig_bit = in_abs_D[2];
+            end
+            12'b0000_001z_zzzz:
+            begin
+                out_E = 3'b010;
+                out_F = in_abs_D[5:2];
+                out_sig_bit = in_abs_D[1];
+            end
+            12'b0000_0001_zzzz:
+            begin
+                out_E = 3'b001;
+                out_F = in_abs_D[4:1];
+                out_sig_bit = in_abs_D[0];
+            end
+            default:
+            begin
+                out_E = 3'b000;
+                out_F = in_abs_D[3:0];
+                out_sig_bit = 0; // no significant bit behind the significand
+            end
+        endcase
+    end
+endmodule
 
-module counter (/*AUTOARG*/
-   // Outputs
-   exponent,
-   // Inputs
-   magnitude, clk
-   );
-	
-	// Misc.
-   input  [11:0] magnitude;
-	output [2:0]  exponent;
-	
-	reg [3:0] zero_count;
-   
-   // Logic
-   input         clk;                  // 100MHz
-	
-	// Assignment
-	initial
-	begin
-		zero_count = 0;
-	end
-	
-	always @(posedge clk)
-	begin
-		zero_count = 0;
-		for (i=11; i>=0; i = i-1)
-		begin
-			if (!magnitude[i])
-			begin
-				zero_count <= zero_count + 1;
-			end
-			else
-			begin
-				i <= -1;
-			end
-		end
-		
-		if (zero_count >= 8)
-		begin
-			exponent <= 0;
-		end
-		else
-		begin
-			exponent <= (zero_count == 7 ? 1 : (zero_count == 6 ? 2 : (zero_count == 5 ? 3 : (zero_count == 4 ? 4 : (zero_count == 3 ? 5 : (zero_count == 2 ? 6 : (zero_count == 1 ? 7 : 8)))))));
-		end
-	end
-	
-endmodule // counter
+// given 3 bit exponent, 4 bit significand, 
+// and the significant bit after the significand
+// round the number either up or down
+module getRoundedFloat(
+    input [2:0] in_E,
+    input [3:0] in_F,
+    input in_sig_bit,
+    output reg [2:0] out_E,
+    output reg [3:0] out_F
+    );
 
-module extracter (/*AUTOARG*/
-   // Outputs
-   significand, fifth,
-   // Inputs
-   magnitude, clk
-   );
-	
-	// Misc.
-   input  [11:0] magnitude;
-   output [3:0]  significand;
-	output        fifth;
-	
-	reg [3:0] push_reg;
-   
-   // Logic
-   input         clk;                  // 100MHz
-	
-	// Assignment
-	initial
-	begin
-		push_reg = 0;
-	end
-	
-	always @(posedge clk)
-	begin
-		for (i=11; i>=0; i = i-1)
-		begin
-			if (i <= 3)
-			begin
-				push_reg = magnitude[3:0];
-				fifth = 0;
-			end
-			else
-			begin
-				push_reg = magnitude[i:i-3];
-				if (push_reg[3])
-				begin
-					fifth = magnitude[i-4];
-					i = -1;
-				end
-			end
-		end
-	end
-	
-endmodule // converter
+    reg [4:0] overflow_F;
+    reg [3:0] overflow_E;
+    
+    always @(*)
+    begin
+        // add the significant bit after the significand
+        // to the float representation
+        overflow_F = in_F + in_sig_bit;
+        // if F overflows then increase the exponent by 1
+        overflow_E = in_E + overflow_F[4];
+        // divide by 2 if overflowed
+        out_F = overflow_F >> overflow_F[4];
+        // truncate the exponent
+        out_E = overflow_E[2:0];
+        
+        // catch the edge case where exponent overflows
+        // 12'b0111_1111_111
+        if ( overflow_E[3] )
+        begin
+            out_F = 4'b1111;
+            out_E = 3'b111;
+        end
+    end
+endmodule
+
