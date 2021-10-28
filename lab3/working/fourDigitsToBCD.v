c7011337228d2f7564cbd364b3db903118cdeb3d
William Randall <15082470+randallwc@users.noreply.github.com>
2021-10-28 16:01:09 -0400

    update var names

--- lab3/working/fourDigitsToBCD.v
+++ lab3/working/fourDigitsToBCD.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    18:33:13 10/28/2021 
-// Design Name: 
-// Module Name:    fourDigitsToBCD 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
 module fourDigitsToBCD(
     input [5:0] in_minute,
     input [5:0] in_second,
@@ -58,29 +39,15 @@ module fourDigitsToBCD(
     always @(*)
     begin
         reg_bcds = 0;
-        
         reg_digit_1 = in_minute / 10;
-//        reg_bcds = {reg_bcds[20:0], reg_bcd_1};
-//      $display("DIGIT: %b", reg_digit);
-//        $display("BCD: %b", reg_bcd);
-        
         reg_digit_2 = in_minute % 10;
-//      reg_bcds = {reg_bcds[20:0], reg_bcd_2};
-//      $display("DIGIT: %b", reg_digit);
-        
         reg_digit_3 = in_second / 10;
-//      reg_bcds = {reg_bcds[20:0], reg_bcd_3};
-//      $display("DIGIT: %b", reg_digit);
-        
         reg_digit_4 = in_second % 10;
-//      reg_bcds = {reg_bcds[20:0], reg_bcd_4};
-//      $display("DIGIT: %b", reg_digit);
         reg_bcds = {reg_bcd_1,reg_bcd_2,reg_bcd_3,reg_bcd_4};
     end
-    
+
+    // connect wires
     assign out_bcds = reg_bcds;
-
-
 endmodule
 
 module digitToBCD(
@@ -91,50 +58,48 @@ module digitToBCD(
     always @(*)
     begin
         case (in_digit)
-            0:
-            begin
-                $display("TEST");
+            0 : begin
                 out_bcd = 7'b0000001;
-            end
-            1:
-            begin
+		end
+
+            1 : begin
                 out_bcd = 7'b1001111;
-            end
-            2:
-            begin
+		end
+
+            2 : begin
                 out_bcd = 7'b0010010;
-            end
-            3:
-            begin
+		end
+
+            3 : begin
                 out_bcd = 7'b0000110;
-            end
-            4:
-            begin
+		end
+
+            4 : begin
                 out_bcd = 7'b1001100;
-            end
-            5:
-            begin
+		end
+
+            5 : begin
                 out_bcd = 7'b0100100;
-            end
-            6:
-            begin
+		end
+
+            6 : begin
                 out_bcd = 7'b0100000;
-            end
-            7:
-            begin
+		end
+
+            7 : begin
                 out_bcd = 7'b0001111;
-            end
-            8:
-            begin
+		end
+
+            8 : begin
                 out_bcd = 7'b0000000;
-            end
-            9:
-            begin
+		end
+
+            9 : begin
                 out_bcd = 7'b0000100;
-            end
+		end
+
             default:
                 out_bcd = 7'b1111111;
         endcase
     end
-    
 endmodule
