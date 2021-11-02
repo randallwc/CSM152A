59ad83b2a25e9e83c27b3fa2a604aca7d76a994a
William Randall <15082470+randallwc@users.noreply.github.com>
2021-11-02 04:40:38 -0400

    start working on lab3

---
+++ lab3/src/clockCounter.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+module clockCounter(
+    input wire in_rst,
+    input wire in_norm_clk,
+    input wire in_adj_clk,
+    input wire in_sel,
+    input wire in_adj,
+    input wire in_pause,
+    output wire [5:0] out_minute,
+    output wire [5:0] out_second
+    );
+
+
+endmodule
