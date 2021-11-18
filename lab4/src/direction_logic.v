037a2d08697d29606351b70a502b898dc8ba7af4
William Randall <15082470+randallwc@users.noreply.github.com>
2021-11-18 14:33:04 -0800

    whitespace fixes

--- lab4/src/direction_logic.v
+++ lab4/src/direction_logic.v
@@ -7,28 +7,28 @@ module direction_logic(
     input wire in_button_right,
     output [4:0] out_direction
     );
-    
+
     reg [4:0] m_direction;
-    
+
     initial begin
         m_direction = 5'b00001;
     end
-    
+
     always @(posedge in_button_up) begin
         m_direction = 5'b10000;
     end
-    
+
     always @(posedge in_button_down) begin
         m_direction = 5'b01000;
     end
-    
+
     always @(posedge in_button_left) begin
         m_direction = 5'b00100;
     end
-    
+
     always @(posedge in_button_up) begin
         m_direction = 5'b00010;
     end
-    
+
     assign out_direction = m_direction;
 endmodule
--- lab4/src/snake_logic.v
+++ lab4/src/snake_logic.v
@@ -8,33 +8,33 @@ module snake_logic(
     output wire [9*SIZE:0] out_snakeX,
     output wire [8*SIZE:0] out_snakeY
     );
-    
+
 
     integer i;
-    
+
     reg [9:0] m_snakeX[SIZE:0];
     reg [8:0] m_snakeY[SIZE:0];
-    
+
     initial begin
         for (i = 0; i <= SIZE; i = i+1) begin
             m_snakeX[i] = 320;
             m_snakeY[i] = 240;
         end
     end
-    
+
     always @(posedge in_reset) begin
         for (i = 0; i <= SIZE; i = i+1) begin
             m_snakeX[i] = 320;
             m_snakeY[i] = 240;
         end
     end
-    
+
     always @(posedge in_update_clock) begin
         for (i = SIZE; i > 0; i = i-1) begin
             m_snakeX[i] = m_snakeX[i-1];
             m_snakeX[i] = m_snakeX[i-1];
         end
-        
+
         case (in_direction)
             5'b10000: m_snakeY[0] = m_snakeY[0] - 10;
             5'b01000: m_snakeY[0] = m_snakeY[0] + 10;
@@ -42,7 +42,7 @@ module snake_logic(
             5'b00010: m_snakeX[0] = m_snakeX[0] + 10;
         endcase
     end
-    
+
     generate
         genvar j;
         for (j=0; j<SIZE; j=j+1) begin: stage
@@ -50,5 +50,5 @@ module snake_logic(
             assign out_snakeY[j+:8] = m_snakeY[i];
         end
     endgenerate
-    
+
 endmodule
